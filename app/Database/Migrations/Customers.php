983c698ac06cbd6ef41a6bf2aab5494b4cc703f0
wgb-code <front.willian@gmail.com>
2024-10-14 21:54:54 -0300

    Estruturando: Model, Controller & Migrations dos Customers.

---
+++ app/Database/Migrations/Customers.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Customers extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'BIGINT',
+                'constraint'     => 20,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '60',
+                'unique'     => true,
+            ],
+            'status' => [
+                'type'       => 'ENUM',
+                'constraint' => ['A', 'I'],
+                'default'    => 'A',
+            ],
+            'admission_date' => [
+                'type' => 'DATE',
+            ],
+            'created_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+            'updated_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('customers');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('customers');
+    }
+}
