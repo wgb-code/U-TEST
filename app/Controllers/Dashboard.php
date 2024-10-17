b7f825fef32edb35454f4889d2ab485d087adb4c
wgb-code <front.willian@gmail.com>
2024-10-17 08:01:09 -0300

    WIP on feature/models
    Controllers: Criando método para cadastro de clientes.

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -18,7 +18,7 @@ class Dashboard extends BaseController
     public function index(): string
     {
         $page  = $this->request->getVar('page') ?: 1;
-        $limit = 1;
+        $limit = 10;
 
         $getCustomers   = $this->customersModel->getAllCustomers($page, $limit);
         $totalCustomers = $this->customersModel->countAllCustomers();
@@ -50,6 +50,59 @@ class Dashboard extends BaseController
 
     public function createCustomer()
     {
-        return null;
+        $name          = $this->request->getPost('nameCustomer');
+        $email         = $this->request->getPost('emailCustomer');
+        $admissionDate = $this->request->getPost('createCustomer');
+
+
+        if ((empty($name)) || (empty($email)) || (empty($admissionDate))) {
+            return $this->response->setJSON([
+                'status'  => 'error',
+                'message' => 'Por favor, preencha todos os campos.',
+            ]);
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return $this->response->setJSON([
+                'status' => 'error',
+                'message' => 'Por favor, forneça um e-mail válido.',
+            ]);
+        }
+
+        $checkDate = $this->dashboardHelper->checkDateIsValid($admissionDate);
+
+        if (!$checkDate) {
+            return $this->response->setJSON([
+                'status' => 'error',
+                'message' => 'Por favor, insira uma data entre 01-01-204 e 16-10-2024',
+            ]);
+        }
+
+        $customerInfos = [
+            'name'           => $name,
+            'email'          => $email,
+            'admission_date' => $admissionDate,
+            'status'         => 'C'
+        ];
+
+        $newCustomerId = $this->customersModel->postNewCustomer($customerInfos);
+
+        if ($newCustomerId) {
+            return $this->response->setJSON([
+                'status'  => 'success',
+                'message' => 'Cliente cadastrado com sucesso.',
+                'data'    => [
+                    'id'   => $newCustomerId,
+                    'name' => $name,
+                    'email' => $email
+                ]
+            ]);
+
+        } else {
+            return $this->response->setJSON([
+                'status' => 'error',
+                'message' => 'Erro ao cadastrar o cliente. Por favor, tente novamente.',
+            ]);
+        }
     }
 }
