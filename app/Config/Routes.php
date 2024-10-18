87e91863f84723b0186cd844f2c430c19835ca73
wgb-code <front.willian@gmail.com>
2024-10-17 22:03:38 -0300

    Rotas necessárias para o funcionamento da aplicação.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Dashboard::index');
+$routes->get('Dashboard/index/(:num)', 'Dashboard::index/$1');
+$routes->post('Dashboard/editCustomer', 'Dashboard::editCustomer');
 $routes->post('Dashboard/createCustomer', 'Dashboard::createCustomer');
+$routes->post('Dashboard/deleteCustomer', 'Dashboard::deleteCustomer');
